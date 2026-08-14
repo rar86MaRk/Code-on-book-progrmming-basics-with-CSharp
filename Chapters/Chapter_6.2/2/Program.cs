a77edfb0a3f32e9c3a10523dfae61aef82f5970d
rar86MaRk <rarmark86@google.com>
2026-08-14 21:24:58 +0300

    Added exercise 2

---
+++ Chapters/Chapter_6.2/2/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+int halfN = n - 2;
+
+for (int i = 1; i <= halfN; i++)
+{
+    if(i % 2 != 0){Console.WriteLine(new string('*', halfN) + "\\ /" + new string('*', halfN));}else{Console.WriteLine(new string('-', halfN) + "\\ /" + new string('-', halfN));}
+}
+
+Console.WriteLine(new string(' ',halfN + 1) + '@');
+
+for (int i = 1; i <= halfN; i++)
+{
+    if(i % 2 != 0){Console.WriteLine(new string('*', halfN) + "/ \\" + new string('*', halfN));}else{Console.WriteLine(new string('-', halfN) + "/ \\" + new string('-', halfN));}
+}
