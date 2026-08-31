7b0d8a34ad519e70baac2a283298bde8adadcd1f
rar86MaRk <rarmark86@google.com>
2026-08-31 15:54:46 +0300

    Added chapter 7.1, exercise 1,2,3,4,5,6.

---
+++ Chapters/Chapter_7.1/4/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+int num = 0;
+
+for (int i = 0; i <= n; i++)
+{
+    var res = Math.Pow(2,num);
+
+    if (num % 2 == 0)
+    {
+        Console.WriteLine(res);
+    }
+
+    num = num + 1;
+}
---
+++ Chapters/Chapter_7.1/6/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+while(n < 1 || n > 100)
+{
+    Console.WriteLine("Invalid number!");
+    n = int.Parse(Console.ReadLine()!);
+}
+
+Console.WriteLine("This number is: "+n);
